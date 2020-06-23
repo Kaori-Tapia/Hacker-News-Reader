5d16a7690bd4697126c98d74422d8a30b7fdce80
Kaori-Tapia <kaoritapiak@gmail.com>
2020-06-23 11:36:38 -0700

    Updates

---
+++ precache-manifest.a650e0c04e92316e8e00b412b5c43b1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852a3cf2c3d8e3141351dba8c8aceeba",
+    "url": "/Kaori-Tapia.github.io/Hacker-News-Reader/index.html"
+  },
+  {
+    "revision": "79a6f428c7bdbc6d9531",
+    "url": "/Kaori-Tapia.github.io/Hacker-News-Reader/static/js/2.137944af.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/Kaori-Tapia.github.io/Hacker-News-Reader/static/js/2.137944af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d136071c4e3fa586aff9",
+    "url": "/Kaori-Tapia.github.io/Hacker-News-Reader/static/js/main.0a8f3a90.chunk.js"
+  },
+  {
+    "revision": "b18cf52894de883c08ba",
+    "url": "/Kaori-Tapia.github.io/Hacker-News-Reader/static/js/runtime-main.a59556b5.js"
+  }
+]);
